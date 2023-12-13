2ceafdf80b32560727c9f04902876748afb69509
Tan Jing En <jingen0463@gmail.com>
2023-12-13 14:51:56 +0800

    create post

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ProfileController;
+use App\Http\Controllers\API\PostController;
 
+
+//user
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
@@ -11,6 +14,13 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('refresh', 'refresh');
 });
 
+//profile photo
 Route::middleware('auth:api')->group(function () {
     Route::post('/profile/update-profile', [ProfileController::class, 'update_profile']);
 });
+
+//post
+Route :: post('posts/create','API\PostController@create');
+Route :: post('posts/delete','API\PostController@delete');
+Route :: post('posts/update','API\PostController@update');
+Route :: post('posts','API\PostController@posts');
