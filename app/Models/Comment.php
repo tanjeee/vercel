58969da9c6758f98838c8560c41e3a494dfa46d4
Tan Jing En <jingen0463@gmail.com>
2023-12-13 14:55:35 +0800

    create post

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(){
+        return $this->belongsTo(Post::class);
+    }
 }
